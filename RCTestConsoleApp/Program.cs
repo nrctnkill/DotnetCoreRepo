538977737defa725f2ec39f831cb3d27e32d5cb1
Ravichandra <nrctnkill@gmail.com>
2022-08-06 19:53:05 +0530

    Intial Commit

---
+++ RCTestConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RCTestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int res = Add(6, 5);
+            Console.WriteLine("Result:"+res);
+        }
+
+        static int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
